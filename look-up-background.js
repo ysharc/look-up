a561573568c8d38d4e5ec3f1d809eb74512ac82f
Yeshwanth Arcot <ysharc@gmail.com>
2017-01-20 00:25:59 +0530

    Version 0.2

---
+++ look-up-background.js
@@ -0,0 +1,51 @@
+//Look-up web-extension
+function getMeaning(url, word, callback) {
+	var client = new XMLHttpRequest();
+	var w = word;
+	client.timeout = 3000;
+	client.addEventListener("load", callback(w));
+	client.ontimeout = timeOut;
+	client.addEventListener("error", transferFailed);
+	client.open("GET", url, true);
+	client.send();
+}
+
+function sendMeaning(word){
+	return function(){
+		var text = this.responseText;
+		contentPort.postMessage({
+			word: word,
+			text: text
+		});
+	};
+}
+
+function timeOut(){
+	contentPort.postMessage({error: "Look-Up:\n\n Network too slow. Please try again later."});
+}
+
+function transferFailed(){
+	contentPort.postMessage({error: "Look-Up:\n\n An error has occured. Please check your connection and try again."});
+}
+
+// =============================================
+// Connection from content script
+// =============================================
+var contentPort;
+
+function connected(port) {
+	contentPort = port;
+	contentPort.onMessage.addListener(function(message) {
+		var words = message.words;
+		if (words.length > 4){
+			contentPort.postMessage({warning: "too many words"});
+		} else{
+			for(var i in words){
+				if (words[i])
+					getMeaning("http://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" + words[i] + "###############################", words[i], sendMeaning);
+			}
+		}
+	});
+}
+
+browser.runtime.onConnect.addListener(connected);
---
+++ look-up-content.js
@@ -0,0 +1,130 @@
+var read_mode = true;
+var auto_mode = false;
+var listener_set = false;
+var doc = document;
+
+function onError(error){
+	alert(error);
+}
+
+function change_settings(){
+	var look_up_existing = browser.storage.local.get("look_up");
+	look_up_existing.then((res) => {
+		read_mode = res[0].look_up.mode;
+		auto_mode = res[0].look_up.auto;
+		if(read_mode === "read" && !listener_set){
+			doc.addEventListener("mouseup", handleText, false);
+			listener_set = true;
+		} else if(read_mode === "silent" && listener_set) {
+			doc.removeEventListener("mouseup", handleText, false);
+			listener_set = false;
+		}
+	}, onError);
+}
+
+var look_up_existing = browser.storage.local.get("look_up");
+look_up_existing.then((res) => {
+	if(!res[0].look_up){
+		//look_up settings have not been set yet.
+		// Initialize with read_mode and auto:false
+		var look_up = {
+			mode: "read",
+			auto: false
+		};
+		var setting = browser.storage.local.set({look_up});
+		setting.then(change_settings, onError);
+	} else {
+		change_settings();
+	}
+	browser.storage.onChanged.addListener(change_settings);
+}, onError);
+
+function handleText(){
+	var text = getSelectedText();
+	var words = text.split(" ");
+	if (words.length > 25 && auto_mode){
+		var look_up = {
+			read_mode: "silent",
+			auto: true
+		};
+		var setting = browser.storage.local.set({look_up});
+		setting.then(function(){
+			doc.removeEventListener("mouseup", handleText, false);
+			listener_set = false;
+		}, onError);
+	}
+	get_meaning(words);
+}
+
+function getSelectedText() {
+	if (window.getSelection) {
+		return window.getSelection().toString();
+	} else if (document.selection) {
+		return document.selection.createRange().text;
+	}
+	return "";
+}
+
+function notifyMeaning(word, text) {
+	var meaning = JSON.parse(text);
+	var notification_text = "";
+	var error = false;
+	for (var i in meaning.results){
+		if(meaning.results[i].part_of_speech && meaning.results[i].senses[0].definition){
+			notification_text += meaning.results[i].part_of_speech.toString() + ": " + meaning.results[i].senses[0].definition[0].toString();
+			notification_text += "\n\n";
+		} else if(meaning.results[i].senses[0].definition){
+			notification_text += meaning.results[i].headword.toString() + ": " + meaning.results[i].senses[0].definition[0].toString();
+			notification_text += "\n\n";
+		}
+	}
+	if(!notification_text){
+		error = true;
+		notification_text = "Sorry we couldn't find the word " + word + " in pearson dictionary.\n\n Please click this notification to know the meaning"; 
+	}
+	if (Notification.permission === "granted") {
+		// If it's okay let's create a notification
+		
+		var notification = new Notification(word, {
+			body: notification_text
+		});
+		if(error){
+			notification.onclick = function(){
+				var external = window;
+				external.open("http://google.com/search?q="+word);
+				external.focus();
+			};
+		}
+	} else {
+		Notification.requestPermission(function (permission) {
+			// If the user accepts, let's create a notification
+			if (permission === "granted") {
+				var notification = new Notification(word, {
+					body: notification_text
+				});
+				if(error){
+					notification.onclick = function(){
+						window.open("http://google.com/search?q="+word);
+						window.focus();
+					};
+				}
+			}
+		});
+	}
+}
+
+// =============================================
+// Connection to background script
+// =============================================
+var port = browser.runtime.connect();
+
+function get_meaning(word){
+	port.postMessage({words: word});
+}
+
+port.onMessage.addListener(function(message) {
+	if (message.error)
+		alert(message.error);
+	if (message.word)
+		notifyMeaning(message.word, message.text);
+});
